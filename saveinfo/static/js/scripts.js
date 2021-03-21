dc352f33e1e0f41f0ce82c28a0fa2401ef866156
Getode <62245111+Getode@users.noreply.github.com>
2021-03-21 19:05:02 +0200

    Add files via upload

---
+++ saveinfo/static/js/scripts.js
@@ -0,0 +1,27 @@
+jQuery(function($){
+
+    $('.search_block_link').click(function(){
+        $('.search_block form').toggleClass('show');
+        $('.has-sub_menu a').removeClass('show');
+        $('.sub_menu.full_width').removeClass('show');
+    });
+    
+    $('.has-sub_menu a').click(function(){
+        $('.sub_menu.full_width').toggleClass('show');
+        $(this).toggleClass('show')
+        $('.search_block_link').removeClass('show');
+        $('.search_wrap form.').removeClass('show');
+    });
+    
+    $('.burger').click(function(){
+        $('.top_menu>ul').toggleClass('show');
+    });
+    
+    $('.header_info_link').click(function(){
+        $('.modal_form_overflow, .modal_form').addClass('show');
+    });
+    $('.modal_form_overflow,.modal_close').click(function(){
+        $('.modal_form_overflow, .modal_form').removeClass('show');
+        $('.sub_menu.full_width').removeClass('show');
+    });
+});
